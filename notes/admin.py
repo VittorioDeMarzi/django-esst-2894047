17828ed45c95e4f01abb4114ae0a5ff0331ac885
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-09-16 12:54:52 +0200

    add Notes startapp implement class notes in models attach the model to admin model implement authorized html add pattern to urls add authorized function to home views (login_required)

---
+++ notes/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from . import models
+
+# Register your models here.
+class NotesAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+#that model is attached to this admin model
+admin.site.register(models.Notes, NotesAdmin)
